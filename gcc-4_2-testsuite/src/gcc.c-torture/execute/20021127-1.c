8a8913ebd59369549b195593e53aa0b6f6a4d262
JP Bonn <jpbonn@JPs-MacBook-Pro.local>
2015-04-13 16:16:27 +0100

    Added comment.

--- gcc-4_2-testsuite/src/gcc.c-torture/execute/20021127-1.c
+++ gcc-4_2-testsuite/src/gcc.c-torture/execute/20021127-1.c
@@ -1,3 +1,8 @@
+/* This is expected to be optimized out.  Apple clang
+   does it at -O1 or better. 
+This is a gcc-ism and the behavior is undefined.
+See bug http://llvm.org/bugs/show_bug.cgi?id=9556
+*/
 long long a = -1;
 long long llabs (long long);
 void abort (void);
